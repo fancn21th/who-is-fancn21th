6ade75401d138ea9114f5dbc8ab0342284bd357d
fancn21th <fancn21th@gmail.com>
2022-11-07 14:31:07 +0800

    welcome message

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 #!/usr/bin/env node
+const { name: title, description, version } = require("./package.json");
+const welcome = require("cli-welcome");
+
+welcome({
+  title,
+  description,
+  tagLine: `hello there`,
+  version,
+  bgColor: `#FADC00`,
+  color: `#000000`,
+  bold: true,
+  clear: false,
+});
+
 console.log(`
 Fan, Yi-Jie
 
