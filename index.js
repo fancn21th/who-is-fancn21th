4025c75b8fc25fe419db3bdcfc582327ec271ea2
fancn21th <fancn21th@gmail.com>
2022-11-08 19:20:22 +0800

    take argv with meow

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 #!/usr/bin/env node
-
+const meow = require('meow');
 const init = require('./utils/init');
-const {bio, social, alerts} = require('./utils/data');
+const alert = require('./utils/alert');
+const {bio, social} = require('./utils/data');
 
 // log
 const log = console.log;
+
+const options = {
+	flags: {
+		social: {
+			type: 'boolean',
+			default: true
+		}
+	}
+};
+
+const cli = meow(`RUN: npx fancn21th`, options);
 
 (() => {
 	init();
 
 	log(bio);
-	log(social);
-	log(alerts);
+
+	cli.flags.social && log(social);
+
+	alert({
+		type: 'info',
+		name: 'CLI ARGV',
+		message: 'See Below'
+	});
+
+	log(`cli input`, cli.input);
+	log(`cli flags`, cli.flags);
 })();
--- utils/data.js
+++ utils/data.js
@@ -5,39 +5,18 @@ const secondaryColor = chalk.hex('#6cc644').bold.inverse;
 const textColor = chalk.dim;
 const italicTextColor = chalk.italic;
 
-// alerts
-const symbols = require('log-symbols');
-const success = chalk.green.inverse;
-const info = chalk.blue.inverse;
-const warning = chalk.keyword('orange').inverse;
-const error = chalk.red.bold.inverse;
-
-// output
-
-const bio = `
-${primaryColor(' Fan, Yi-Jie ')}
+// bio
+const bio = `${primaryColor(' Fan, Yi-Jie ')}
 
 ${italicTextColor(
 	'Software engineer @神州数码. Used to work @HP. Love art and design of good things. Wonder loving programming or not. Into Math in a strange way. After comedies tv series or sci-fi movies. Enjoy different genres of music.'
-)}`;
+)}\n`;
 
-const social = `
-${primaryColor(' Github ')}:   ${textColor('https://github.com/fancn21th')}
-${secondaryColor(' Website ')}:  ${textColor('https://fancn21th.cn')}
-`;
-
-const alerts = `
-${symbols.success} ${success(' Success ')} Thanks for viewing my bio.
-
-${symbols.info} ${info(' Info ')} this command cli is very enlightened by Awais.
-
-${symbols.warning} ${warning(' Warning ')} Ready for challenge.
-
-${symbols.error} ${error(' Error ')} I'm out of space.
-`;
+// social
+const social = `${primaryColor(' Github ')}:   ${textColor('https://github.com/fancn21th')}
+${secondaryColor(' Website ')}:  ${textColor('https://fancn21th.cn')}\n`;
 
 module.exports = {
 	bio,
-	social,
-	alerts
+	social
 };
