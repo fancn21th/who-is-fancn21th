6b2e6bb54be9de05889f7ffe85ced96c56eab37c
fancn21th <fancn21th@gmail.com>
2022-11-08 10:35:04 +0800

    err handler and node ver check

--- index.js
+++ index.js
@@ -1,51 +1,42 @@
 #!/usr/bin/env node
-const { name: title, description, version } = require("./package.json");
-const welcome = require("cli-welcome");
-const chalk = require("chalk");
+const chalk = require('chalk');
+const init = require('./utils/init');
+// log
 const log = console.log;
 // colors
-const primaryColor = chalk.hex("#1da1f2").bold.inverse;
-const secondaryColor = chalk.hex("#6cc644").bold.inverse;
+const primaryColor = chalk.hex('#1da1f2').bold.inverse;
+const secondaryColor = chalk.hex('#6cc644').bold.inverse;
 const textColor = chalk.dim;
 const italicTextColor = chalk.italic;
 // alerts
-const symbols = require("log-symbols");
+const symbols = require('log-symbols');
 const success = chalk.green.inverse;
 const info = chalk.blue.inverse;
-const warning = chalk.keyword("orange").inverse;
+const warning = chalk.keyword('orange').inverse;
 const error = chalk.red.bold.inverse;
 
-welcome({
-  title,
-  description,
-  tagLine: `hello there`,
-  version,
-  bgColor: `#1da1f2`,
-  color: `#000000`,
-  bold: true,
-  clear: false,
-});
+init();
 
 log(
-  `${primaryColor(" Fan, Yi-Jie ")}
+	`${primaryColor(' Fan, Yi-Jie ')}
 
 ${italicTextColor(
-  "Software engineer @神州数码. Used to work @HP. Love art and design of good thins. Wonder loving programming or not. Into Math in a strange way. After comedies tv series or sci-fi movies. Enjoy different genres of music."
+	'Software engineer @神州数码. Used to work @HP. Love art and design of good thins. Wonder loving programming or not. Into Math in a strange way. After comedies tv series or sci-fi movies. Enjoy different genres of music.'
 )}
 
-${primaryColor(" Github ")}:   ${textColor("https://github.com/fancn21th")}
+${primaryColor(' Github ')}:   ${textColor('https://github.com/fancn21th')}
 
-${secondaryColor(" Website ")}:  ${textColor("https://fancn21th.cn")}
+${secondaryColor(' Website ')}:  ${textColor('https://fancn21th.cn')}
 `
 );
 
 log(
-  `${symbols.success} ${success(" Success ")} Thanks for viewing my bio.
+	`${symbols.success} ${success(' Success ')} Thanks for viewing my bio.
 
-${symbols.info} ${info(" Info ")} this command cli is very enlightened by Awais.
+${symbols.info} ${info(' Info ')} this command cli is very enlightened by Awais.
 
-${symbols.warning} ${warning(" Warning ")} Ready for challenge.
+${symbols.warning} ${warning(' Warning ')} Ready for challenge.
 
-${symbols.error} ${error(" Error ")} I'm out of space.
+${symbols.error} ${error(' Error ')} I'm out of space.
 `
 );
---
+++ utils/init.js
@@ -0,0 +1,21 @@
+const {name: title, description, version} = require('../package.json');
+const welcome = require('cli-welcome');
+const checkNode = require('cli-check-node');
+const unhandled = require('cli-handle-unhandled');
+
+module.exports = () => {
+	unhandled();
+
+	welcome({
+		title,
+		description,
+		tagLine: `hello there`,
+		version,
+		bgColor: `#1da1f2`,
+		color: `#000000`,
+		bold: true,
+		clear: true
+	});
+
+	checkNode('12');
+};
